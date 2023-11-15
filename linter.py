dacd09f2cc5deb98c0b6d7c90bfd17f12ed0d837
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-11-15 14:25:13 +0100

    linter

---
+++ linter.py
@@ -0,0 +1,14 @@
+import sys
+from pylint import lint
+
+THRESHOLD = 10
+run = lint.Run(["--rcfile=.pylintrc", "main.py"], exit=False)
+score = run.linter.stats.global_note
+
+print(f"Score: {score}")
+
+if score < THRESHOLD:
+    print(f"Linter failed, Score:{score} < Threshold value? {THRESHOLD}")
+    sys.exit(1)
+sys.exit(0)
+    
